1ca821934e1003372956a25d60dd055e6938e7da
Valerija <vvalerija@gmail.com>
2022-09-16 00:29:24 +0300

    Add five empty test methods into Siusk24.java

---
+++ src/test/java/lt/valerija/bd/Siusk24.java
@@ -0,0 +1,38 @@
+package lt.valerija.bd;
+
+import org.testng.annotations.Test;
+
+public class Siusk24 {
+    @Test
+    public void priceCountFunction(){
+
+
+    }
+
+    @Test
+
+    public void logInWithNonExistingAccountInfo(){
+
+    }
+
+    @Test
+
+    public void logInWithExistingAccountInfo(){
+
+    }
+
+    @Test
+
+    public void userLogOut(){
+
+
+    }
+
+    @Test
+
+    public void parcelSearch(){
+
+    }
+
+}
+
